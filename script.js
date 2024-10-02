648df382990cf2241847de16c4a86414a99d7177
Manikanta8887 <swamimanikanta66@gmail.com>
2024-10-02 13:04:40 +0530

    Adding Files

---
+++ script.js
@@ -0,0 +1,115 @@
+// 1st Question
+
+var a=[1,2,3,4,5];
+console.log(a);
+a.push(6);
+console.log(a);
+
+//  2nd Question
+
+var a1=[1,2,3,4,5];
+console.log(a1);
+a1.pop(5);
+console.log(a1);
+
+//  3rd Question
+
+var a2=[1,2,3,4,5];
+console.log(a2);
+a2.unshift(1);
+console.log(a2);
+
+//  4th Question
+
+var a3=[1,2,3,4,5];
+console.log(a2);
+a3.shift(1);
+console.log(a3);
+
+//  5th Question
+
+var a4=[1,2,3,4,5];
+console.log(a4);
+a4.reverse();
+console.log(a4);
+
+//  6th Question
+
+var a5=['m','a','n','i','k','a','n','t','a'];
+console.log(a5);
+var a6=a5.indexOf('k');
+console.log(a6);
+
+//  7th Question
+
+var a7=['m','a','n','i','k','a','n','t','a'];
+console.log(a7);
+var a8=a7.includes('k');
+console.log(a8);
+
+//  8th Question
+
+var a9=['m','a','n','i','k','a','n','t','a'];
+console.log(a9);
+var a10=a9.sort();
+console.log(a10);
+
+//  9th Question
+
+var a11=["Manikanta","is", "a", "FrontEnd Developer"];
+console.log(a11);
+var a12=a11.toString(","); // var a12=a11.join(",");
+console.log(a12);
+
+//  Questions witrh Multiple Array Methods
+
+// 1st Question
+
+var b=[0,1,2,3,4,5];
+console.log(b);
+b.push(6);
+console.log(b);
+b.shift(0);
+console.log(b);
+
+// 2nd Question
+
+var b1=["Manikanta","is","FrontEnd Developer"];
+console.log(b1);
+var b2=b1.reverse();
+console.log(b2);
+var b3=b1.join(",");
+console.log(b3);
+
+//  3rd Question
+
+var c=['m','a','n','i','k','a','n','t','a'];
+console.log(c);
+var c1=c.sort();
+console.log(c1);
+c1.pop();
+console.log(c1);
+
+//  4th Question
+
+var d=['m','a','n','i','k','a','n','t','a'];
+console.log(d);
+d.push("t","s");
+console.log(d);
+d.shift();
+console.log(d);
+
+//  5th Question
+
+var m=['m','a','n','i','k','a','n','t','a'];
+var t=['t','h','i','r','n','a','t','i'];
+console.log(d);
+var mt=m.concat(t);
+console.log(mt);
+mt.pop();
+console.log(mt);
+
+
+
+
+
